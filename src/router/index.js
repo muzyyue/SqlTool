0b5861dad247854e207b8b011531f2d3260f6674
ling <2253373466@qq.com>
2025-12-08 17:03:52 +0800

    修复打包后，文件无法运行的err

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: () => import('../views/Home.vue')
+    component: () => import('../views/Home.vue'),
   },
   {
     path: '/insert',
     name: 'insert',
-    component: () => import('../views/insert.vue')
+    component: () => import('../views/insert.vue'),
   },
   {
     path: '/update',
     name: 'update',
-    component: () => import('../views/update.vue')
-  }
+    component: () => import('../views/update.vue'),
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
 })
 
 export default router
